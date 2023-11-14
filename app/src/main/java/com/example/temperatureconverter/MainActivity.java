823347ae641ff95543f9763e57d698ff385e0cee
Anshul Dohare <anshuldohare6904@gmail.com>
2023-11-14 18:05:41 +0530

    Update MainActivity.java

--- app/src/main/java/com/example/temperatureconverter/MainActivity.java
+++ app/src/main/java/com/example/temperatureconverter/MainActivity.java
@@ -6,7 +6,6 @@ import android.text.TextWatcher;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
@@ -46,8 +45,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
                 unit1 = tempUnit[position];
-                input_number.setText("");
-                result.setText("");
+                getResult();
             }
 
             @Override
@@ -59,8 +57,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
                 unit2 = tempUnit[position];
-                input_number.setText("");
-                result.setText("");
+                getResult();
             }
 
             @Override
@@ -82,166 +79,171 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void afterTextChanged(Editable s) {
-                String input = input_number.getText().toString().trim();
-                if(input.isEmpty()){
-                    result.setText("");
-                    return;
-                }
-                double number = Double.parseDouble(input);
-                double calculate;
-                String res;
-
-                if(unit1.equals("Celsius")){
-                    switch (unit2) {
-                        case "Celsius":
-                            res = input + " °C";
-                            result.setText(res);
-                            break;
-                        case "Fahrenheit":
-                            calculate = number * (9 / 5.0) + 32;
-                            res = df.format(calculate) + " °F";
-                            result.setText(res);
-                            break;
-                        case "Kelvin":
-                            calculate = number + 273.15;
-                            res = df.format(calculate) + " K";
-                            result.setText(res);
-                            break;
-                        case "Rankine":
-                            calculate = (number * 9 / 5) + 491.67;
-                            res = df.format(calculate) + " °R";
-                            result.setText(res);
-                            break;
-                        case "Reaumur":
-                            calculate = number * 0.8;
-                            res = df.format(calculate) + " °Re";
-                            result.setText(res);
-                            break;
-                        default:
-                            Toast.makeText(MainActivity.this, "Something went wrong, Try Again...", Toast.LENGTH_SHORT).show();
-                    }
-                }
-                else if (unit1.equals("Fahrenheit")) {
-                    switch (unit2) {
-                        case "Celsius":
-                            calculate = (number - 32) * 5 / 9;
-                            res = df.format(calculate) + " °C";
-                            result.setText(res);
-                            break;
-                        case "Fahrenheit":
-                            res = input + " °F";
-                            result.setText(res);
-                            break;
-                        case "Kelvin":
-                            calculate = ((number - 32) * 5 / 9) + 273.15;
-                            res = df.format(calculate) + " K";
-                            result.setText(res);
-                            break;
-                        case "Rankine":
-                            calculate = number+459.67;
-                            res = df.format(calculate) + " °R";
-                            result.setText(res);
-                            break;
-                        case "Reaumur":
-                            calculate = (number-32)*4/9;
-                            res = df.format(calculate) + " °Re";
-                            result.setText(res);
-                            break;
-                        default:
-                            Toast.makeText(MainActivity.this, "Something went wrong, Try Again...", Toast.LENGTH_SHORT).show();
-                    }
-                }
-                else if(unit1.equals("Kelvin")){
-                    switch (unit2) {
-                        case "Celsius":
-                            calculate = number - 273.15;
-                            res = df.format(calculate) + " °C";
-                            result.setText(res);
-                            break;
-                        case "Fahrenheit":
-                            calculate = (number - 273.15) * 9 / 5 + 32;
-                            res = df.format(calculate) + " °F";
-                            result.setText(res);
-                            break;
-                        case "Kelvin":
-                            res = input + " K";
-                            result.setText(res);
-                            break;
-                        case "Rankine":
-                            calculate = number*1.8;
-                            res = df.format(calculate) + " °R";
-                            result.setText(res);
-                            break;
-                        case "Reaumur":
-                            calculate = (number - 273.15) * 0.8;
-                            res = df.format(calculate) + " °Re";
-                            result.setText(res);
-                            break;
-                        default:
-                            Toast.makeText(MainActivity.this, "Something went wrong, Try Again...", Toast.LENGTH_SHORT).show();
-                    }
-                }
-                else if (unit1.equals("Rankine")){
-                    switch (unit2) {
-                        case "Celsius":
-                            calculate = ( number - 32 - 459.67) / 1.8;
-                            res = df.format(calculate) + " °C";
-                            result.setText(res);
-                            break;
-                        case "Fahrenheit":
-                            calculate =  number - 459.67;
-                            res = df.format(calculate) + " °F";
-                            result.setText(res);
-                            break;
-                        case "Kelvin":
-                            calculate = number/1.8;
-                            res = df.format(calculate) + " K";
-                            result.setText(res);
-                            break;
-                        case "Rankine":
-                            res = input + " °R";
-                            result.setText(res);
-                            break;
-                        case "Reaumur":
-                            calculate =( number - 32 - 459.67) / 2.25;
-                            res = df.format(calculate) + " °Re";
-                            result.setText(res);
-                            break;
-                        default:
-                            Toast.makeText(MainActivity.this, "Something went wrong, Try Again...", Toast.LENGTH_SHORT).show();
-                    }
-                }
-                else if (unit1.equals("Reaumur")) {
-                    switch (unit2) {
-                        case "Celsius":
-                            calculate = number * 1.25;
-                            res = df.format(calculate) + " °C";
-                            result.setText(res);
-                            break;
-                        case "Fahrenheit":
-                            calculate =  (number * 2.25 )+ 32;
-                            res = df.format(calculate) + " °F";
-                            result.setText(res);
-                            break;
-                        case "Kelvin":
-                            calculate = (number * 1.25) + 273.15;
-                            res = df.format(calculate) + " K";
-                            result.setText(res);
-                            break;
-                        case "Rankine":
-                            calculate = (number*  2.25) +459.67+32;
-                            res = df.format(calculate) + " °R";
-                            result.setText(res);
-                            break;
-                        case "Reaumur":
-                            res = input + " °Re";
-                            result.setText(res);
-                            break;
-                        default:
-                            Toast.makeText(MainActivity.this, "Something went wrong, Try Again...", Toast.LENGTH_SHORT).show();
-                    }
-                }
+                getResult();
             }
         });
+    }
+    public void getResult(){
+        String input = input_number.getText().toString().trim();
+        if(input.isEmpty()){
+            result.setText("");
+            return;
+        }
+        double number = Double.parseDouble(input);
+        double calculate;
+        String res;
+
+        switch (unit1) {
+            case "Celsius":
+                switch (unit2) {
+                    case "Celsius":
+                        res = input + " °C";
+                        result.setText(res);
+                        break;
+                    case "Fahrenheit":
+                        calculate = number * (9 / 5.0) + 32;
+                        res = df.format(calculate) + " °F";
+                        result.setText(res);
+                        break;
+                    case "Kelvin":
+                        calculate = number + 273.15;
+                        res = df.format(calculate) + " K";
+                        result.setText(res);
+                        break;
+                    case "Rankine":
+                        calculate = (number * 9 / 5) + 491.67;
+                        res = df.format(calculate) + " °R";
+                        result.setText(res);
+                        break;
+                    case "Reaumur":
+                        calculate = number * 0.8;
+                        res = df.format(calculate) + " °Re";
+                        result.setText(res);
+                        break;
+                    default:
+                        Toast.makeText(MainActivity.this, "Something went wrong, Try Again...", Toast.LENGTH_SHORT).show();
+                }
+                break;
+            case "Fahrenheit":
+                switch (unit2) {
+                    case "Celsius":
+                        calculate = (number - 32) * 5 / 9;
+                        res = df.format(calculate) + " °C";
+                        result.setText(res);
+                        break;
+                    case "Fahrenheit":
+                        res = input + " °F";
+                        result.setText(res);
+                        break;
+                    case "Kelvin":
+                        calculate = ((number - 32) * 5 / 9) + 273.15;
+                        res = df.format(calculate) + " K";
+                        result.setText(res);
+                        break;
+                    case "Rankine":
+                        calculate = number + 459.67;
+                        res = df.format(calculate) + " °R";
+                        result.setText(res);
+                        break;
+                    case "Reaumur":
+                        calculate = (number - 32) * 4 / 9;
+                        res = df.format(calculate) + " °Re";
+                        result.setText(res);
+                        break;
+                    default:
+                        Toast.makeText(MainActivity.this, "Something went wrong, Try Again...", Toast.LENGTH_SHORT).show();
+                }
+                break;
+            case "Kelvin":
+                switch (unit2) {
+                    case "Celsius":
+                        calculate = number - 273.15;
+                        res = df.format(calculate) + " °C";
+                        result.setText(res);
+                        break;
+                    case "Fahrenheit":
+                        calculate = (number - 273.15) * 9 / 5 + 32;
+                        res = df.format(calculate) + " °F";
+                        result.setText(res);
+                        break;
+                    case "Kelvin":
+                        res = input + " K";
+                        result.setText(res);
+                        break;
+                    case "Rankine":
+                        calculate = number * 1.8;
+                        res = df.format(calculate) + " °R";
+                        result.setText(res);
+                        break;
+                    case "Reaumur":
+                        calculate = (number - 273.15) * 0.8;
+                        res = df.format(calculate) + " °Re";
+                        result.setText(res);
+                        break;
+                    default:
+                        Toast.makeText(MainActivity.this, "Something went wrong, Try Again...", Toast.LENGTH_SHORT).show();
+                }
+                break;
+            case "Rankine":
+                switch (unit2) {
+                    case "Celsius":
+                        calculate = (number - 32 - 459.67) / 1.8;
+                        res = df.format(calculate) + " °C";
+                        result.setText(res);
+                        break;
+                    case "Fahrenheit":
+                        calculate = number - 459.67;
+                        res = df.format(calculate) + " °F";
+                        result.setText(res);
+                        break;
+                    case "Kelvin":
+                        calculate = number / 1.8;
+                        res = df.format(calculate) + " K";
+                        result.setText(res);
+                        break;
+                    case "Rankine":
+                        res = input + " °R";
+                        result.setText(res);
+                        break;
+                    case "Reaumur":
+                        calculate = (number - 32 - 459.67) / 2.25;
+                        res = df.format(calculate) + " °Re";
+                        result.setText(res);
+                        break;
+                    default:
+                        Toast.makeText(MainActivity.this, "Something went wrong, Try Again...", Toast.LENGTH_SHORT).show();
+                }
+                break;
+            case "Reaumur":
+                switch (unit2) {
+                    case "Celsius":
+                        calculate = number * 1.25;
+                        res = df.format(calculate) + " °C";
+                        result.setText(res);
+                        break;
+                    case "Fahrenheit":
+                        calculate = (number * 2.25) + 32;
+                        res = df.format(calculate) + " °F";
+                        result.setText(res);
+                        break;
+                    case "Kelvin":
+                        calculate = (number * 1.25) + 273.15;
+                        res = df.format(calculate) + " K";
+                        result.setText(res);
+                        break;
+                    case "Rankine":
+                        calculate = (number * 2.25) + 459.67 + 32;
+                        res = df.format(calculate) + " °R";
+                        result.setText(res);
+                        break;
+                    case "Reaumur":
+                        res = input + " °Re";
+                        result.setText(res);
+                        break;
+                    default:
+                        Toast.makeText(MainActivity.this, "Something went wrong, Try Again...", Toast.LENGTH_SHORT).show();
+                }
+                break;
+        }
     }
 }
